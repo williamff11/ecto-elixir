985f55ee25300f6a66b3cbeea2695c0f1cb4d08a
William Freire <williamff11@gmail.com>
2020-10-12 23:16:27 -0400

    query

--- lib/friends/movie.ex
+++ lib/friends/movie.ex
@@ -1,11 +1,22 @@
 defmodule Friends.Movie do
   use Ecto.Schema
 
+  import Ecto.Changeset
+
   schema "movies" do
     field :title, :string
     field :tagline, :string
     has_many :characters, Friends.Character
     has_one :distributor, Friends.Distributor
     many_to_many :actors, Friends.Actor, join_through: "movies_actors"
+  end
+
+  def changeset(struct, params) do
+    struct
+    |>cast(params, [:title, :tagline])
+    |>validate_required([:title, :tagline])
+    |>validate_length(:title, min: 2)
+    |>validate_length(:tagline, min: 10, max: 15)
+    |>Friends.Repo.insert()
   end
 end
